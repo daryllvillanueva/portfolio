3d1de852c11b78e6d6358bf16489b5c933c4ee5e
Daryll <darylljvillanueva@gmail.com>
2024-06-24 18:54:29 +0800

    fix thumbnail

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -19,15 +19,15 @@ const Home = () => {
   return (
     <main className='bg-black'>
       <Header/>
-      <section className='banner relative' id='banner'>
+      <section className='banner relative py-4 sm:py-0' id='banner'>
         <Banner/>
-        <button type='button' className='absolute bottom-3 left-1/2 hidden lg:block' onClick={() => scrollToElement("tools")}>
+        <button type='button' className='absolute bottom-0 lg:bottom-3 left-1/2 hidden md:block' onClick={() => scrollToElement("tools")}>
           <FaArrowDown className='text-white size-9'/>
         </button>
       </section>
-      <section className='tools relative' id='tools'>
+      <section className='tools relative pt-[2rem] sm:pt-0' id='tools'>
         <Tools scrollToElement={scrollToElement}/>
-        <button type='button' className='absolute bottom-5 left-1/2 hidden lg:block' onClick={() => scrollToElement("projects")}>
+        <button type='button' className='absolute bottom-4 left-1/2 hidden md:block' onClick={() => scrollToElement("projects")}>
           <FaArrowDown className='text-white size-9'/>
         </button>
       </section>
--- src/components/pages/banner/Banner.jsx
+++ src/components/pages/banner/Banner.jsx
@@ -5,11 +5,11 @@ import { FaArrowDown } from "react-icons/fa";
 
 const Banner = () => {
   return (
-    <main className='h-screen animate-fade-in'>
+    <main className='sm:h-screen animate-fade-in'>
       <div className='h-full w-full max-w-[90rem] flex flex-col lg:flex-row justify-evenly items-center gap-5 lg:gap-0 px-3'>
         <div className='flex flex-col gap-3 items-center w-full text-center lg:text-start lg:items-start lg:w-auto order-2 lg:order-1'>
           <div>
-            <h2 className='text-2xl sm:text-4xl text-white/90 font-semibold text-left font-kanit_m'>Hello, I'm </h2>
+            <h2 className='text-2xl sm:text-4xl text-white/90 font-semibold text-center lg:text-left font-kanit_m'>Hello, I'm </h2>
             <h1 className='block text-4xl sm:text-5xl md:text-6xl gradient-text font-semibold font-kanit_m'>Daryll Villanueva!</h1>
           </div>
           <p className='text-lg sm:text-xl max-w-[30rem] font-medium text-white/80 tracking-wider text-center lg:text-left font-kanit_m'>
