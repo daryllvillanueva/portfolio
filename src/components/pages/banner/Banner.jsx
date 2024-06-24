dc36c516be1356b1503031ab4584e1254677b07b
Daryll <darylljvillanueva@gmail.com>
2024-06-25 01:15:58 +0800

    minor fix

--- src/components/pages/banner/Banner.jsx
+++ src/components/pages/banner/Banner.jsx
@@ -11,8 +11,7 @@ const Banner = () => {
             <h1 className='block text-4xl sm:text-5xl md:text-6xl gradient-text font-semibold font-kanit_m'>Daryll Villanueva!</h1>
           </div>
           <p className='text-lg sm:text-xl max-w-[30rem] font-medium text-white/80 tracking-wider text-center lg:text-left font-kanit_m'>
-            My passion lies in crafting websites that not only captivate users but also provide seamless functionality. I'm a fresh grad that strive to break into the tech industry that allows me to gain knowledge
-            and make an impact.
+            My passion lies in crafting websites that not only captivate users but also provide seamless functionality. I'm a fresh grad striving to break into the tech industry, where I can gain knowledge and make an impact.
           </p>
         </div>
         <div className='h-[18rem] w-[18rem] overflow-hidden lg:h-[26rem] lg:w-[26rem] p-2 bg-gray-200 rounded-full hover:shadow-custom order-1 lg:order-2 mt-20 lg:mt-0'>
